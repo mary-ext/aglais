e26eb32e77163f6fef9461f48bbc4f8d0af80012
Mary <git@mary.my.id>
2024-06-24 17:21:07 +0700

    feat: some vs every guard

--- src/lib/hooks/guard.ts
+++ src/lib/hooks/guard.ts
@@ -1,21 +1,12 @@
 import { createMemo, createSignal, onCleanup } from 'solid-js';
 
 export type GuardFunction = () => boolean;
+type GuardKind = 'some' | 'every';
 
-export const createGuard = (memoize = false) => {
+export const createGuard = (kind: GuardKind = 'some', memoize = false) => {
 	const [guards, setGuards] = createSignal<GuardFunction[]>([]);
 
-	const isGuarded = createMemoMaybe(memoize, () => {
-		const $guards = guards();
-		for (let idx = 0, len = $guards.length; idx < len; idx++) {
-			const guard = $guards[idx];
-			if (guard()) {
-				return true;
-			}
-		}
-
-		return false;
-	});
+	const isGuarded = createMemoMaybe(memoize, createIsGuarded(kind, guards));
 
 	const addGuard = (guard: GuardFunction) => {
 		setGuards((guards) => guards.concat(guard));
@@ -23,6 +14,38 @@ export const createGuard = (memoize = false) => {
 	};
 
 	return [isGuarded, addGuard] as const;
+};
+
+const createIsGuarded = (kind: GuardKind, guards: () => GuardFunction[]) => {
+	if (kind === 'every') {
+		return () => {
+			const $guards = guards();
+
+			for (let idx = 0, len = $guards.length; idx < len; idx++) {
+				const guard = $guards[idx];
+				if (!guard()) {
+					return false;
+				}
+			}
+
+			return true;
+		};
+	} else if (kind === 'some') {
+		return () => {
+			const $guards = guards();
+
+			for (let idx = 0, len = $guards.length; idx < len; idx++) {
+				const guard = $guards[idx];
+				if (guard()) {
+					return true;
+				}
+			}
+
+			return false;
+		};
+	}
+
+	return () => false;
 };
 
 const createMemoMaybe = <T>(memoize: boolean, fn: () => T): (() => T) => {
