1fd25f85f74bd1740cd967939cc4226724238eac
Mary <git@mary.my.id>
2024-06-28 22:26:48 +0700

    feat: extra link embeds

--- src/components/embeds/external-embed.tsx
+++ src/components/embeds/external-embed.tsx
@@ -1,5 +1,14 @@
+import { createEffect, createSignal } from 'solid-js';
+
 import type { AppBskyEmbedExternal } from '@mary/bluesky-client/lexicons';
+
 import { safeUrlParse } from '~/api/utils/strings';
+
+import CircularProgress from '../circular-progress';
+import PlaySolidIcon from '../icons-central/play-solid';
+
+import { SnippetType, detectSnippet } from './lib/snippet';
+import { on } from '~/lib/misc';
 
 export interface ExternalEmbedProps {
 	/** Expected to be static */
@@ -14,10 +23,118 @@ const ExternalEmbed = ({ embed, interactive }: ExternalEmbedProps) => {
 	const url = safeUrlParse(uri);
 	const domain = trimDomain(url?.host ?? '');
 
+	const snippet = detectSnippet(embed.external);
+	const type = snippet.type;
+
+	if (interactive && type === SnippetType.BLUESKY_GIF) {
+		const [playing, setPlaying] = createSignal(false);
+		const [stalling, setStalling] = createSignal(false);
+
+		let _stallTimeout: number | undefined;
+
+		return (
+			<div
+				class="relative max-h-80 max-w-full self-start overflow-hidden rounded-md border border-outline"
+				style={/* @once */ { 'aspect-ratio': snippet.ratio }}
+			>
+				<video
+					ref={(node) => {
+						createEffect(() => {
+							if (playing()) {
+								node.play();
+							} else if (!node.paused) {
+								node.pause();
+								node.currentTime = 0;
+							}
+						});
+					}}
+					tabindex={-1}
+					poster={thumb}
+					src={/* @once */ snippet.url}
+					loop
+					onWaiting={() => {
+						clearTimeout(_stallTimeout);
+						_stallTimeout = setTimeout(() => setStalling(true), 50);
+					}}
+					onPlaying={() => {
+						clearTimeout(_stallTimeout);
+						setStalling(false);
+					}}
+					class="h-full w-full"
+				/>
+
+				{/* FIXME: this is the same hack as standalone images in image embeds */}
+				<div class="h-screen w-screen"></div>
+
+				<div hidden={!(!playing() || stalling())} class="absolute inset-0 bg-black/50"></div>
+
+				<button
+					title={!playing() ? 'Play GIF' : `Pause GIF`}
+					aria-description={/* @once */ snippet.description}
+					onClick={() => setPlaying(!playing())}
+					class="absolute inset-0 grid place-items-center rounded-md outline-2 -outline-offset-2 outline-white focus-visible:outline"
+				>
+					{!playing() ? (
+						<div class="grid h-9 w-9 place-items-center rounded-full border-2 border-white bg-accent">
+							<PlaySolidIcon class="text-sm" />
+						</div>
+					) : stalling() ? (
+						<CircularProgress size={32} />
+					) : null}
+				</button>
+			</div>
+		);
+	}
+
+	if (interactive && type === SnippetType.IFRAME) {
+		const [show, setShow] = createSignal(false);
+
+		return on(show, (isShowing) => {
+			if (isShowing) {
+				return (
+					<div
+						class="overflow-hidden rounded-md border border-outline"
+						style={/* @once */ { 'aspect-ratio': snippet.ratio }}
+					>
+						<iframe src={/* @once */ snippet.url} allow="autoplay; fullscreen" class="h-full w-full" />
+					</div>
+				);
+			}
+
+			return (
+				<div class="flex overflow-hidden rounded-md border border-outline">
+					<button
+						onClick={() => setShow(true)}
+						class="relative aspect-square w-[86px] shrink-0 border-r border-outline"
+					>
+						{thumb && <img src={thumb} class="h-full w-full object-cover" />}
+
+						<div class="absolute inset-0 grid place-items-center">
+							<div class="grid h-9 w-9 place-items-center rounded-full border-2 border-white bg-accent">
+								<PlaySolidIcon class="text-sm" />
+							</div>
+						</div>
+					</button>
+
+					<a
+						href={uri}
+						target="_blank"
+						rel="noopener noreferrer nofollow"
+						class="flex min-w-0 flex-col justify-center gap-0.5 p-3 text-sm hover:bg-contrast/sm active:bg-contrast/sm-pressed"
+					>
+						<p class="overflow-hidden text-ellipsis text-contrast-muted empty:hidden">{domain}</p>
+						<p class="line-clamp-2 break-words font-medium empty:hidden">{title}</p>
+					</a>
+				</div>
+			);
+		});
+	}
+
 	return (
 		<a
 			href={interactive ? uri : undefined}
 			target="_blank"
+			rel="noopener noreferrer nofollow"
 			class={
 				`flex overflow-hidden rounded-md border border-outline` +
 				(interactive ? ` hover:bg-contrast/sm active:bg-contrast/sm-pressed` : ``)
---
+++ src/components/embeds/lib/snippet.ts
@@ -0,0 +1,128 @@
+import type { AppBskyEmbedExternal } from '@mary/bluesky-client/lexicons';
+
+import { safeUrlParse } from '~/api/utils/strings';
+
+export const enum SnippetType {
+	LINK,
+	BLUESKY_GIF,
+	IFRAME,
+}
+
+export const enum SnippetSource {
+	YOUTUBE,
+	SOUNDCLOUD,
+}
+
+interface LinkSnippet {
+	type: SnippetType.LINK;
+	/** Domain name */
+	domain?: string;
+}
+
+interface BlueskyGifSnippet {
+	type: SnippetType.BLUESKY_GIF;
+	/** Video URL */
+	url: string;
+	/** Aspect ratio */
+	ratio: string;
+	/** Alt text description */
+	description: string;
+}
+
+interface IframeSnippet {
+	type: SnippetType.IFRAME;
+	/** Source type */
+	source: SnippetSource;
+	/** Source domain */
+	domain: string;
+	/** Iframe URL */
+	url: string;
+	/** Aspect ratio */
+	ratio: string;
+}
+
+export type Snippet = LinkSnippet | BlueskyGifSnippet | IframeSnippet;
+
+export const detectSnippet = (
+	link: Pick<AppBskyEmbedExternal.ViewExternal, 'uri' | 'description'>,
+): Snippet => {
+	const url = link.uri;
+	const u = safeUrlParse(url);
+
+	if (u === null) {
+		return { type: SnippetType.LINK };
+	}
+
+	const h = u.host;
+	const p = u.pathname;
+	const q = u.searchParams;
+
+	const d = h.startsWith('www.') ? h.slice(4) : h;
+
+	let m: RegExpExecArray | null | undefined;
+
+	if (d === 'media.tenor.com') {
+		// Bluesky GIFs
+		if ((m = /\/([^/]+?AAAAC)\/([^/]+?)\?hh=(\d+?)&ww=(\d+?)$/.exec(url))) {
+			const id = m[1].replace(/AAAAC$/, 'AAAP3');
+			const file = m[2].replace(/\.gif$/, '.webm');
+
+			const width = m[4];
+			const height = m[3];
+
+			return {
+				type: SnippetType.BLUESKY_GIF,
+				url: `https://t.gifs.bsky.app/${id}/${file}`,
+				ratio: `${width}/${height}`,
+				description: link.description.replace(/^(ALT|Alt): /, ''),
+			};
+		}
+	} else if (d === 'youtube.com' || d === 'm.youtube.com' || d === 'music.youtube.com') {
+		// YouTube iframe
+		if (p === '/watch') {
+			const videoId = q.get('v');
+			const seek = q.get('t') || 0;
+
+			return {
+				type: SnippetType.IFRAME,
+				source: SnippetSource.YOUTUBE,
+				domain: d,
+				url: `https://www.youtube-nocookie.com/embed/${videoId}?start=${seek}&autoplay=1&playsinline=1`,
+				ratio: d !== 'music.youtube.com' ? `16/9` : `1`,
+			};
+		}
+	} else if (d === 'youtu.be') {
+		// YouTube iframe
+		if ((m = /^\/([^/]+?)$/.exec(p))) {
+			const videoId = m[1];
+			const seek = encodeURIComponent(q.get('t') || 0);
+
+			return {
+				type: SnippetType.IFRAME,
+				source: SnippetSource.YOUTUBE,
+				domain: d,
+				url: `https://www.youtube-nocookie.com/embed/${videoId}?start=${seek}&autoplay=1&playsinline=1`,
+				ratio: `16/9`,
+			};
+		}
+	} else if (d === 'soundcloud.com' || d === 'www.soundcloud.com') {
+		// SoundCloud embed
+		if ((m = /^\/([^/]+?)(?:\/(?!reposts$)([^/]+?)|\/sets\/([^/]+?))?$/.exec(p))) {
+			return {
+				type: SnippetType.IFRAME,
+				source: SnippetSource.SOUNDCLOUD,
+				domain: d,
+				url: `https://w.soundcloud.com/player/?url=${encodeURIComponent(url)}&auto_play=true&visual=false&hide_related=true`,
+				ratio: m[3] ? `1/1` : `16/9`,
+			};
+		}
+	}
+
+	{
+		// Link snippet, always matches
+		return {
+			type: SnippetType.LINK,
+			domain: d,
+		};
+	}
+};
