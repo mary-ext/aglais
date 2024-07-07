0ce60d75f39cd780249452ca08ba4f7314b9033e
Mary <git@mary.my.id>
2024-07-07 15:51:43 +0700

    refactor: separate all bookmarks

--- src/views/bookmarks.tsx
+++ src/views/bookmarks.tsx
@@ -1,7 +1,8 @@
-import { For } from 'solid-js';
+import { For, Match, Switch } from 'solid-js';
 
 import { createQuery } from '@mary/solid-query';
 
+import type { TagItem } from '~/lib/aglais-bookmarks/db';
 import { formatCompact } from '~/lib/intl/number';
 import { useBookmarks } from '~/lib/states/bookmarks';
 
@@ -11,51 +12,39 @@ import ChevronRightOutlinedIcon from '~/components/icons-central/chevron-right-o
 import FolderAddOutlinedIcon from '~/components/icons-central/folder-add-outline';
 import * as Page from '~/components/page';
 
-interface TagEntry {
-	id: number | string;
-	name: string;
-	color?: string;
-	icon?: string;
+interface HydratedTagItem extends TagItem {
 	count: number;
 }
 
 const BookmarksPage = () => {
 	const bookmarks = useBookmarks();
 
-	const entries = createQuery(() => {
+	const query = createQuery(() => {
 		return {
 			queryKey: ['bookmarks'],
 			staleTime: 30_000,
-			async queryFn(): Promise<TagEntry[]> {
+			async queryFn() {
 				const db = await bookmarks.open();
 				const tx = db.transaction(['tags', 'bookmarks'], 'readonly');
 
 				const tags = await tx.objectStore('tags').getAll();
 				const bookmarksStore = tx.objectStore('bookmarks');
 
-				const counts = await Promise.all([
+				const [totalCount, ...counts] = await Promise.all([
 					bookmarksStore.count(),
 					...tags.map((tag) => {
 						return bookmarksStore.index('tags').count(tag.id);
 					}),
 				]);
 
-				return [
-					{
-						id: 'all',
-						name: 'All bookmarks',
-						count: counts[0],
-					},
-					...tags.map((tag, idx): TagEntry => {
-						return {
-							id: tag.id,
-							name: tag.name,
-							color: tag.color,
-							icon: tag.icon,
-							count: counts[idx + 1],
-						};
-					}),
-				];
+				const hydrated = tags.map((tag, idx): HydratedTagItem => {
+					return {
+						...tag,
+						count: counts[idx],
+					};
+				});
+
+				return { totalCount, tags: hydrated };
 			},
 		};
 	});
@@ -74,27 +63,51 @@ const BookmarksPage = () => {
 				</Page.HeaderAccessory>
 			</Page.Header>
 
-			<For each={entries.data}>
-				{(entry) => {
-					return (
-						<a
-							href={/* @once */ `/bookmarks/${entry.id}`}
-							class="flex items-center gap-4 px-4 py-3 hover:bg-contrast/sm active:bg-contrast/sm-pressed"
-						>
-							<BookmarkFolderAvatar color={/* @once */ entry.color} icon={/* @once */ entry.icon} />
+			<Switch>
+				<Match when={query.data}>
+					{(data) => (
+						<>
+							<a
+								href="/bookmarks/all"
+								class="flex items-center gap-4 px-4 py-3 hover:bg-contrast/sm active:bg-contrast/sm-pressed"
+							>
+								<BookmarkFolderAvatar />
 
-							<span class="min-w-0 grow overflow-hidden text-ellipsis whitespace-nowrap break-words text-sm font-bold">
-								{/* @once */ entry.name}
-							</span>
+								<span class="min-w-0 grow overflow-hidden text-ellipsis whitespace-nowrap break-words text-sm font-bold">
+									All Bookmarks
+								</span>
 
-							<div class="flex shrink-0 items-center gap-3">
-								<span class="text-sm text-contrast-muted">{/* @once */ formatCompact(entry.count)}</span>
-								<ChevronRightOutlinedIcon class="shrink-0 text-xl text-contrast-muted" />
-							</div>
-						</a>
-					);
-				}}
-			</For>
+								<div class="flex shrink-0 items-center gap-3">
+									<span class="text-sm text-contrast-muted">{formatCompact(data().totalCount)}</span>
+									<ChevronRightOutlinedIcon class="shrink-0 text-xl text-contrast-muted" />
+								</div>
+							</a>
+
+							<For each={data().tags}>
+								{(entry) => (
+									<a
+										href={/* @once */ `/bookmarks/${entry.id}`}
+										class="flex items-center gap-4 px-4 py-3 hover:bg-contrast/sm active:bg-contrast/sm-pressed"
+									>
+										<BookmarkFolderAvatar color={/* @once */ entry.color} icon={/* @once */ entry.icon} />
+
+										<span class="min-w-0 grow overflow-hidden text-ellipsis whitespace-nowrap break-words text-sm font-bold">
+											{/* @once */ entry.name}
+										</span>
+
+										<div class="flex shrink-0 items-center gap-3">
+											<span class="text-sm text-contrast-muted">
+												{/* @once */ formatCompact(entry.count)}
+											</span>
+											<ChevronRightOutlinedIcon class="shrink-0 text-xl text-contrast-muted" />
+										</div>
+									</a>
+								)}
+							</For>
+						</>
+					)}
+				</Match>
+			</Switch>
 		</>
 	);
 };
