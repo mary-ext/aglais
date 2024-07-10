d691efbc9cbe62b5102522e726df80133fa9cef3
Mary <git@mary.my.id>
2024-07-10 07:47:45 +0700

    refactor: don't precache handle

--- src/api/queries-cache/profile-precache.ts
+++ src/api/queries-cache/profile-precache.ts
@@ -3,5 +3,4 @@ import type { QueryClient } from '@mary/solid-query';
 
 export const precacheProfile = (queryClient: QueryClient, profile: AppBskyActorDefs.ProfileViewBasic) => {
 	queryClient.setQueryData(['profile-precache', profile.did], profile);
-	queryClient.setQueryData(['profile-precache', profile.handle], profile);
 };
