e9f0911bbf224acaf268a3ecd264b4c657dd3c64
Mary <git@mary.my.id>
2024-07-16 11:02:47 +0700

    feat: make appearance settings functional

--- src/views/settings-appearance.tsx
+++ src/views/settings-appearance.tsx
@@ -1,7 +1,16 @@
+import * as preferences from '~/globals/preferences';
+
+import { useSession } from '~/lib/states/session';
+
 import * as Boxed from '~/components/boxed';
 import * as Page from '~/components/page';
 
 const AppearanceSettingsPage = () => {
+	const { currentAccount } = useSession();
+
+	const uiPrefs = preferences.global.ui;
+	const threadViewPrefs = currentAccount!.preferences.threadView;
+
 	return (
 		<>
 			<Page.Header>
@@ -17,35 +26,46 @@ const AppearanceSettingsPage = () => {
 					<Boxed.List>
 						<Boxed.SelectItem
 							label="Application theme"
-							value={'system'}
-							onChange={(next) => {
-								//
-							}}
+							value={uiPrefs.theme}
+							onChange={(next) => (uiPrefs.theme = next)}
 							options={[
-								{
-									value: 'system',
-									label: `System default`,
-								},
-								{
-									value: 'light',
-									label: `Light`,
-								},
-								{
-									value: 'dark',
-									label: `Dark`,
-								},
+								{ value: 'system', label: `System default` },
+								{ value: 'light', label: `Light` },
+								{ value: 'dark', label: `Dark` },
 							]}
+						/>
+					</Boxed.List>
+				</Boxed.Group>
+
+				<Boxed.Group>
+					<Boxed.GroupHeader>Thread appearance</Boxed.GroupHeader>
+
+					<Boxed.List>
+						<Boxed.SelectItem
+							label="Sort replies by"
+							value={threadViewPrefs.sort}
+							onChange={(next) => (threadViewPrefs.sort = next)}
+							options={[
+								{ value: 'clout', label: `The Algorithm™` },
+								{ value: 'most-likes', label: `Most liked` },
+								{ value: 'newest', label: `Newest posted` },
+								{ value: 'oldest', label: `Oldest posted` },
+							]}
+						/>
+
+						<Boxed.ToggleItem
+							label="Prioritize replies from people you follow"
+							enabled={threadViewPrefs.followsFirst}
+							onChange={(next) => (threadViewPrefs.followsFirst = next)}
 						/>
 					</Boxed.List>
 
 					<Boxed.List>
 						<Boxed.ToggleItem
 							label="Show post replies in threaded view"
-							description="This is an experimental feature"
-							enabled={true}
-							onChange={(next) => {
-								//
-							}}
+							description="Experimental feature, might change at any time"
+							enabled={threadViewPrefs.treeView}
+							onChange={(next) => (threadViewPrefs.treeView = next)}
 						/>
 					</Boxed.List>
 				</Boxed.Group>
