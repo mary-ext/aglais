4f3e7ad568263718fa7f7edf912c52de1c85e440
Mary <git@mary.my.id>
2024-07-18 13:19:27 +0700

    chore: fix precache types

--- src/api/queries-cache/profile-precache.ts
+++ src/api/queries-cache/profile-precache.ts
@@ -1,6 +1,9 @@
 import type { AppBskyActorDefs } from '@mary/bluesky-client/lexicons';
 import type { QueryClient } from '@mary/solid-query';
 
-export const precacheProfile = (queryClient: QueryClient, profile: AppBskyActorDefs.ProfileViewBasic) => {
+export const precacheProfile = (
+	queryClient: QueryClient,
+	profile: AppBskyActorDefs.ProfileViewBasic | AppBskyActorDefs.ProfileView,
+) => {
 	queryClient.setQueryData(['profile-precache', profile.did], profile);
 };
