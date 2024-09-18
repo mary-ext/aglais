53a6c2ba0233013a1e31c9d194d80112fa6e45c7
Mary <git@mary.my.id>
2024-09-18 13:01:02 +0700

    refactor: remove unused functions

--- src/api/oauth/utils.ts
+++ src/api/oauth/utils.ts
@@ -1,7 +1,5 @@
 import type { UnwrapArray } from '~/api/utils/types';
 
-type FalsyValue = false | 0 | null | undefined;
-
 export const decoder = new TextDecoder();
 export const encoder = new TextEncoder();
 
@@ -9,33 +7,6 @@ export const locks = navigator.locks;
 
 export const extractContentType = (headers: Headers): string | undefined => {
 	return headers.get('content-type')?.split(';')[0];
-};
-
-export const followAbortSignals = (...signals: (AbortSignal | FalsyValue)[]): AbortSignal | undefined => {
-	const filtered = signals.filter((v) => !!v);
-
-	if (filtered.length === 0) {
-		return;
-	}
-	if (filtered.length === 1) {
-		return filtered[0];
-	}
-
-	const controller = new AbortController();
-	const own = controller.signal;
-
-	for (let idx = 0, len = filtered.length; idx < len; idx++) {
-		const signal = filtered[idx];
-
-		if (signal.aborted) {
-			controller.abort(signal.reason);
-			break;
-		}
-
-		signal.addEventListener('abort', () => controller.abort(signal.reason), { signal: own });
-	}
-
-	return own;
 };
 
 export const toBase64Url = (input: Uint8Array): string => {
@@ -65,13 +36,6 @@ export const fromBase64Url = (input: string): Uint8Array => {
 	}
 };
 
-export const parseB64uJson = (input: string) => {
-	const bytes = fromBase64Url(input);
-	const text = decoder.decode(bytes);
-
-	return JSON.parse(text);
-};
-
 export const toSha256 = async (input: string): Promise<string> => {
 	const bytes = encoder.encode(input);
 	const digest = await crypto.subtle.digest('SHA-256', bytes);
@@ -84,10 +48,6 @@ export const randomBytes = (length: number): string => {
 };
 
 export const generateState = (): string => {
-	return randomBytes(16);
-};
-
-export const generateNonce = (): string => {
 	return randomBytes(16);
 };
 
