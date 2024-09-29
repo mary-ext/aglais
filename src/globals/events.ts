f996f8c0ce432f074627564fa8be098eefc1b325
Mary <git@mary.my.id>
2024-09-29 22:56:04 +0700

    refactor: clean up global events

--- src/globals/events.ts
+++ src/globals/events.ts
@@ -1,13 +1,8 @@
 import { EventEmitter } from '@mary/events';
 
 export const globalEvents = new EventEmitter<{
-	// Current session has expired
-	sessionexpired(): void;
 	// User has published a post
 	postpublished(): void;
-	// User tried navigating to the same main page they're already in
-	softreset(): void;
-
 	// Media is being played
 	mediaplay(id: string): void;
 }>();
