270102a8686438f33d78cc9ce31cc40b4f6b2e01
Mary <git@mary.my.id>
2024-10-08 08:58:05 +0700

    fix: mess around with oauth callback page again lol

--- src/views/oauth-callback.tsx
+++ src/views/oauth-callback.tsx
@@ -101,7 +101,7 @@ const OAuthCallbackPage = () => {
 		<Switch>
 			<Match when={resource.error}>
 				{(error) => (
-					<div class="flex max-w-sm grow flex-col items-center justify-center gap-6 p-6">
+					<div class="mx-auto flex max-w-sm grow flex-col items-center justify-center gap-6 p-6">
 						<div class="text-sm">
 							<p class="text-p-red-400">Authentication failed</p>
 							<p class="text-contrast-muted">
@@ -124,7 +124,7 @@ const OAuthCallbackPage = () => {
 			</Match>
 
 			<Match when>
-				<div class="flex max-w-sm grow flex-col items-center justify-center gap-6 p-6">
+				<div class="mx-auto flex max-w-sm grow flex-col items-center justify-center gap-6 p-6">
 					<p class="text-sm text-contrast-muted">Processing your sign in information</p>
 					<CircularProgress />
 				</div>
