426de0816a9f4e2b2b7041264e922f7382953713
Mary <git@mary.my.id>
2024-10-18 21:40:09 +0700

    feat: don't fetch signed-in accounts

--- src/components/main/manage-account-dialog.tsx
+++ src/components/main/manage-account-dialog.tsx
@@ -90,21 +90,24 @@ const CurrentAccountItem = () => {
 };
 
 const AccountItem = ({ account, onClick }: { account: AccountData; onClick?: () => void }) => {
-	const profile = createProfileQuery(() => account.did);
+	const profile = () => account.profile;
 
 	return (
 		<button
 			onClick={onClick}
 			class="flex gap-4 px-4 py-3 text-left hover:bg-contrast/md active:bg-contrast/sm-pressed"
 		>
-			<Avatar type={getUserAvatarType(profile.data)} src={profile.data?.avatar} class="mt-0.5" />
+			<Avatar type={getUserAvatarType(profile())} src={profile()?.avatar} class="mt-0.5" />
 
 			<div class="min-w-0 grow self-center text-sm">
 				<p class="overflow-hidden text-ellipsis whitespace-nowrap font-bold empty:hidden">
-					{profile.data?.displayName}
+					{profile()?.displayName}
 				</p>
 				<p class="overflow-hidden text-ellipsis whitespace-nowrap text-de text-contrast-muted">
-					{'@' + profile.data?.handle}
+					{(() => {
+						const $profile = profile();
+						return $profile && $profile.handle !== 'handle.invalid' ? '@' + $profile.handle : account.did;
+					})()}
 				</p>
 			</div>
 		</button>
