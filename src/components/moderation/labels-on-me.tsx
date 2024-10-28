34f3fe2ad744a7eed56b5debec7929057a73a7ae
Mary <git@mary.my.id>
2024-10-29 06:49:00 +0700

    refactor: adjust it a little bit

--- src/components/moderation/labels-on-me.tsx
+++ src/components/moderation/labels-on-me.tsx
@@ -1,4 +1,4 @@
-import { Show, createMemo } from 'solid-js';
+import { Show } from 'solid-js';
 
 import type { ComAtprotoLabelDefs } from '@atcute/client/lexicons';
 
@@ -13,35 +13,35 @@ export interface LabelsOnMeProps {
 }
 
 const LabelsOnMe = (props: LabelsOnMeProps) => {
-	const count = createMemo(() => {
-		const { currentAccount } = useSession();
-
-		const labels = props.labels;
-		const did = currentAccount?.did;
-
-		if (!labels?.length || !did) {
-			return 0;
-		}
-
-		return labels.filter((l) => l.src !== did && l.val[0] !== '!').length;
-	});
+	const { currentAccount } = useSession();
 
 	return (
-		<Show when={count() > 0}>
-			<div class={props.class}>
-				<button
-					onClick={() => {}}
-					class={
-						`group flex items-center rounded-md text-contrast/75 hover:text-contrast` +
-						(!props.large ? ` h-5 text-xs hover:bg-contrast/sm-pressed` : ` h-6 bg-contrast/10 text-de`)
-					}
-				>
-					<CircleInfoOutlinedIcon
-						class={`ml-1 opacity-80 group-hover:opacity-100` + (!props.large ? ` text-xs` : ` w-4 text-sm`)}
-					/>
-					<span class="mx-1.5">{`${count()} ${count() === 1 ? `label` : `labels`} placed on this content`}</span>
-				</button>
-			</div>
+		<Show
+			when={(() => {
+				const did = currentAccount?.did;
+				const labels = props.labels?.filter((l) => l.src !== did && l.val[0] !== '!');
+
+				if (labels && labels.length > 0) {
+					return labels;
+				}
+			})()}
+		>
+			{(labels) => (
+				<div class={props.class}>
+					<button
+						onClick={() => {}}
+						class={
+							`group flex items-center rounded-md text-contrast/75 hover:text-contrast` +
+							(!props.large ? ` h-5 text-xs hover:bg-contrast/sm-pressed` : ` h-6 bg-contrast/10 text-de`)
+						}
+					>
+						<CircleInfoOutlinedIcon
+							class={`ml-1 opacity-80 group-hover:opacity-100` + (!props.large ? ` text-xs` : ` w-4 text-sm`)}
+						/>
+						<span class="mx-1.5">{`${labels().length} ${labels().length === 1 ? `label` : `labels`} placed on this content`}</span>
+					</button>
+				</div>
+			)}
 		</Show>
 	);
 };
