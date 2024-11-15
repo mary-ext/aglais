e0ac1f123288c27bb15f303827036399ee61e2b4
Mary <git@mary.my.id>
2024-11-15 20:17:35 +0700

    feat: my lists query

---
+++ src/api/queries/my-lists.ts
@@ -0,0 +1,123 @@
+import type { AppBskyGraphDefs } from '@atcute/client/lexicons';
+import { createQuery } from '@mary/solid-query';
+
+import { useAgent } from '~/lib/states/agent';
+import { useSession } from '~/lib/states/session';
+
+export type MyListsFilter = 'all' | 'curation' | 'moderation' | 'all-including-subscribed';
+
+export const createMyListsQuery = (filter: MyListsFilter) => {
+	const { rpc } = useAgent();
+	const { currentAccount } = useSession();
+
+	return createQuery(() => ({
+		queryKey: ['my-lists', filter],
+		async queryFn({ signal }) {
+			const promises = [
+				accumulate(async (cursor) => {
+					const { data } = await rpc.get('app.bsky.graph.getLists', {
+						signal,
+						params: {
+							actor: currentAccount!.did,
+							cursor,
+							limit: 100,
+						},
+					});
+
+					return {
+						cursor: data.cursor,
+						items: data.lists,
+					};
+				}),
+			];
+
+			if (filter === 'all-including-subscribed' || filter === 'moderation') {
+				promises.push(
+					accumulate(async (cursor) => {
+						const { data } = await rpc.get('app.bsky.graph.getListMutes', {
+							signal,
+							params: {
+								cursor,
+								limit: 100,
+							},
+						});
+
+						return {
+							cursor: data.cursor,
+							items: data.lists,
+						};
+					}),
+				);
+
+				promises.push(
+					accumulate(async (cursor) => {
+						const { data } = await rpc.get('app.bsky.graph.getListBlocks', {
+							signal,
+							params: {
+								cursor,
+								limit: 100,
+							},
+						});
+
+						return {
+							cursor: data.cursor,
+							items: data.lists,
+						};
+					}),
+				);
+			}
+
+			const resultset = await Promise.all(promises);
+
+			const collator = new Intl.Collator();
+			const known = new Set<string>();
+
+			const lists: AppBskyGraphDefs.ListView[] = [];
+
+			for (const result of resultset) {
+				result.sort((a, b) => collator.compare(a.name, b.name));
+
+				for (const list of result) {
+					if (
+						(filter === 'curation' && list.purpose !== 'app.bsky.graph.defs#curatelist') ||
+						(filter === 'moderation' && list.purpose !== 'app.bsky.graph.defs#modlist')
+					) {
+						continue;
+					}
+
+					if (known.has(list.uri)) {
+						continue;
+					}
+
+					known.add(list.uri);
+					lists.push(list);
+				}
+			}
+
+			return lists;
+		},
+	}));
+};
+
+interface AccumulateResponse<T> {
+	cursor?: string;
+	items: T[];
+}
+
+type AccumulateFetchFn<T> = (cursor: string | undefined) => Promise<AccumulateResponse<T>>;
+
+const accumulate = async <T>(fn: AccumulateFetchFn<T>, limit = 100): Promise<T[]> => {
+	let cursor: string | undefined;
+	let acc: T[] = [];
+
+	for (let i = 0; i < limit; i++) {
+		const res = await fn(cursor);
+		cursor = res.cursor;
+		acc = acc.concat(res.items);
+		if (!cursor) {
+			break;
+		}
+	}
+
+	return acc;
+};
