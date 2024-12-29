c775817e5e2b721388affa52ebb835663c516b73
Mary <git@mary.my.id>
2024-12-29 22:28:56 +0700

    fix: focus input after clearing

--- src/components/main/search-bar.tsx
+++ src/components/main/search-bar.tsx
@@ -16,6 +16,8 @@ const SearchBar = (props: SearchBarProps) => {
 
 	const reset = () => setSearch(props.value ?? '');
 
+	let inputEl: HTMLInputElement;
+
 	return (
 		<form
 			onSubmit={(ev) => {
@@ -30,6 +32,9 @@ const SearchBar = (props: SearchBarProps) => {
 				class="flex h-7.5 items-center gap-3 rounded-full bg-contrast/10 px-3 outline-2 outline-accent focus-within:bg-background focus-within:outline"
 			>
 				<input
+					ref={(el) => {
+						inputEl = el;
+					}}
 					value={search()}
 					onInput={(ev) => setSearch(ev.target.value)}
 					placeholder="Search"
@@ -40,7 +45,10 @@ const SearchBar = (props: SearchBarProps) => {
 					<button
 						type="button"
 						tabindex={-1}
-						onClick={() => setSearch('')}
+						onClick={() => {
+							setSearch('');
+							inputEl!.focus();
+						}}
 						class="text-contrast-muted outline-none hover:text-contrast"
 					>
 						<CrossLargeOutlinedIcon class="text-lg" />
