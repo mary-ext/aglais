86ceb8bc01fa13669a82199ed445fa391d64de89
Mary <git@mary.my.id>
2025-02-28 16:28:20 +0700

    fix: reduce intensity

--- src/views/settings-explore-feeds.tsx
+++ src/views/settings-explore-feeds.tsx
@@ -133,7 +133,7 @@ const ExploreFeedsSettingsPage = () => {
 									ref={(node) => {
 										draggable.refs.element(node);
 									}}
-									class="relative flex shrink-0 cursor-grab select-none items-center gap-4 px-4 py-3 hover:bg-contrast/sm-pressed"
+									class="relative flex shrink-0 cursor-grab select-none items-center gap-4 px-4 py-3 hover:bg-contrast/sm"
 								>
 									<DropIndicator edge={draggable.edge} />
 									<DraggablePreview container={draggable.preview}>
